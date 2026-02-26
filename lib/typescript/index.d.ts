818f700b471e85a671f6c660eab4fe814c3e52f0
shuraman <a.fateev@stayfitt.ru>
2026-02-26 20:29:37 +0300

    update lib/

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -2,3 +2,5 @@ export { default as HoldItem } from './components/holdItem';
 export { default as HoldMenuProvider } from './components/provider';
 export { default as HoldMenuFlatList } from './components/flatList';
 export { default as HoldMenuIcon } from './components/icon';
+export type { HoldItemProps } from './components/holdItem/types';
+export type { MenuItemProps, CustomViewProps, RenderCustomView, } from './components/menu/types';
